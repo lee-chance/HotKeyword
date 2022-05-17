9494c2b0eaa0e7d8e6d6806dccf47384e80b9951
이창수 <cslee@ichangsuui-MacBookPro.local>
2022-05-17 18:57:32 +0900

    Network base 작성

---
+++ HotKeyword/Common/HotKeywordService.swift
@@ -0,0 +1,15 @@
+//
+//  HotKeywordService.swift
+//  HotKeyword
+//
+//  Created by 이창수 on 2022/05/17.
+//
+
+import Foundation
+
+enum HotKeywordService {
+    
+}
+
+//extension HotKeywordService: Service {
+//}
---
+++ HotKeyword/Common/Network/ServiceProvider.swift
@@ -0,0 +1,117 @@
+//
+//  ServiceProvider.swift
+//  HotKeyword
+//
+//  Created by 이창수 on 2022/05/17.
+//
+
+import Foundation
+import Combine
+
+enum NetworkError: Error {
+    case serverError(statusCode: Int?)
+    case noDataReceived
+    case decodingError(DecodingError)
+    case unknowError(Error)
+}
+
+final class ServiceProvider<S: Service> {
+    let session = URLSession.shared
+    
+    init() { }
+    
+    func get<T: Decodable>(service: S, callback: @escaping (Result<T, Error>) -> ()) {
+        session.dataTask(with: service.urlRequest) { data, response, error in
+            // 1. Network Error Handling
+            guard error == nil else {
+                callback(.failure(error!))
+                return
+            }
+            
+            // 2. Server Error Handling
+            let statusCode = (response as? HTTPURLResponse)?.statusCode
+            guard
+                let code = statusCode,
+                (200 ..< 300) ~= code
+            else {
+                callback(.failure(NetworkError.serverError(statusCode: statusCode)))
+                return
+            }
+            
+            // 3. No Data Error
+            guard let data = data else {
+                callback(.failure(NetworkError.noDataReceived))
+                return
+            }
+            
+            do {
+                // 4. Decoding Data
+                let decoded = try JSONDecoder().decode(T.self, from: data)
+                callback(.success(decoded))
+            } catch {
+                // 5. Decoding Error Handling
+                guard let decodingError = error as? DecodingError else {
+                    callback(.failure(NetworkError.unknowError(error)))
+                    return
+                }
+                
+                callback(.failure(NetworkError.decodingError(decodingError)))
+            }
+        }
+        .resume()
+    }
+    
+    func get<T: Decodable>(service: S) -> AnyPublisher<T, Error> {
+        session.dataTaskPublisher(for: service.urlRequest)
+            .tryMap { data, response in
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                guard
+                    let code = statusCode,
+                    (200 ..< 300) ~= code
+                else {
+                    throw NetworkError.serverError(statusCode: statusCode)
+                }
+                
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    @available(iOS 15.0, *)
+    func get<T: Decodable>(service: S) async throws -> T {
+        let result: (data: Data, response: URLResponse) = try await session.data(for: service.urlRequest)
+        
+        let statusCode = (result.response as? HTTPURLResponse)?.statusCode
+        guard
+            let code = statusCode,
+            (200 ..< 300) ~= code
+        else {
+            throw NetworkError.serverError(statusCode: statusCode)
+        }
+        
+        return try JSONDecoder().decode(T.self, from: result.data)
+    }
+    
+//    func load(service: T, completion: @escaping (Result<Data>) -> Void) {
+//        call(service.urlRequest, completion: completion)
+//    }
+    
+//    func load<U>(service: T, decodeType: U.Type, completion: @escaping (Result<U>) -> Void) where U: Decodable {
+//        call(service.urlRequest) { result in
+//            switch result {
+//            case .success(let data):
+//                do {
+//                    let response = try JSONDecoder().decode(decodeType, from: data)
+//                    completion(.success(response))
+//                } catch {
+//                    completion(.failure(error))
+//                }
+//            case .failure(let error):
+//                completion(.failure(error))
+//            case .empty:
+//                completion(.empty)
+//            }
+//        }
+//    }
+}
